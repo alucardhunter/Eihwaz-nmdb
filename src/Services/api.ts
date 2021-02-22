e31acefb9512944994a4b179150349bf74a6bef3
Alucard <spdauditoria@gmail.com>
2021-02-21 23:37:37 -0300

    Api by .env

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -2,10 +2,10 @@ import axios from 'axios'
 import { toast } from 'react-toastify'
 
 const Api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3'
+  baseURL: process.env.REACT_APP_TMDB_API
 })
 
-Api.defaults.headers.common.authorization = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2M2ZjA1OTIyMzkxZjQ4ZjA1YjMwMWQ0MDNmYTFjZSIsInN1YiI6IjYwMjg0NjA3YjAyZjVlMDAzZmRmMTRmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-w-3j6zlCje7us_aM2xMFlnxNA7ftpIUSUS6Hh-XkOc'
+axios.defaults.headers.common.authorization = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2M2ZjA1OTIyMzkxZjQ4ZjA1YjMwMWQ0MDNmYTFjZSIsInN1YiI6IjYwMjg0NjA3YjAyZjVlMDAzZmRmMTRmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-w-3j6zlCje7us_aM2xMFlnxNA7ftpIUSUS6Hh-XkOc`
 
 Api.interceptors.response.use(function (response) {
   // Any status code that lie within the range of 2xx cause this function to trigger
